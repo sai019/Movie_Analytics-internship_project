36a667a8f2231fa77902114d8cec0f3286bb52e5
sai019 <kollusai1997@gmail.com>
2024-01-04 11:11:47 +0530

    License and setup.py updated

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+
+setup(name="Movie_Analytics",
+    version="1.0",
+    description="My Big Data internship project",
+    packages=find_packages(),
+    license="MIT")
